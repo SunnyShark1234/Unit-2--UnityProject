46861d3b9090ac290cec37b8d75516e04d6c6b02
Kyle Blissett <k.blissett@surreyschools.ca>
2022-11-03 11:19:28 -0700

    Commit to main
    Just some mechcanics

---
+++ Week2UnityProject/Assets/Scripts/ObjectColorChanger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColorChanger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ChangeMyColor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ChangeMyColor();
+        }
+    }
+
+    void ChangeMyColor()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+}
